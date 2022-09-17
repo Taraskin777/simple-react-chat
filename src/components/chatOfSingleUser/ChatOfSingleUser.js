3cb951b06e349b25d034152ef15519ecff2cbf9a
Taras Noga <taraskin2012@gmail.com>
2022-09-17 14:12:14 +0300

    add default chat

--- src/components/chatOfSingleUser/ChatOfSingleUser.js
+++ src/components/chatOfSingleUser/ChatOfSingleUser.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./chatofsingleuser.css";
 
 const ChatOfSingleUser = ({ comment, date, avatar }) => {
+
+  
   
   return (
     <div className="single-chat">
       <div className="userphoto">
-        <img src={avatar} alt="" />
+        <img src={avatar ? avatar : "/images/sergio.png"} alt="avatar" />
       </div>
       <div className="comment">{comment}</div>
       <div className="date">{date}</div>
--- src/components/singlechat/SingleChat.js
+++ src/components/singlechat/SingleChat.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ChatOfSingleUser from "../chatOfSingleUser/ChatOfSingleUser";
 import InputForSendMessage from "../inputForSendMessage/InputForSendMessage";
-import { getListOfMessages } from "../../services/httpservices";
+import {
+  commentsToSergio,
+  getListOfMessages,
+  sortedUsers,
+} from "../../services/httpservices";
 import { useState, useEffect } from "react";
 import "./singlechat.css";
 
@@ -9,19 +13,25 @@ const SingleChat = ({ newUrl, name, avatar }) => {
   const [messagesList, setMessagesList] = useState([]);
 
   useEffect(() => {
+    getListOfMessages(commentsToSergio).then((data) => setMessagesList(data));
+  }, [newUrl]);
+
+  useEffect(() => {
     getListOfMessages(newUrl).then((data) => setMessagesList(data));
   }, [newUrl]);
+
+
 
   return (
     <div className="chat">
       <div className="userinfo">
         <div className="user-avatar">
-          <img src={avatar} alt="avatar" />
+          <img src={avatar ? avatar : "/images/sergio.png"} alt="avatar" />
           <div className="user-tick">
             <img src="/images/tick.png" alt="tick" />
           </div>
         </div>
-        <h2>{name}</h2>
+        <h2>{name ? name : "Sergio"}</h2>
       </div>
       <div className="single-chat-wrapper">
         {messagesList.map(({ id, comment, date }) => (
